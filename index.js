14a1ca3681b3449b8aeb9d367ac6ec96c69cdef4
mihaivenczel <mihaivenczel@yahoo.com>
2021-05-22 22:44:27 +0300

    update

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-var express = require('express'),
+const modelData = [
+  {
+    id: 0,
+    title: "Chair 1",
+    description: "Un scaun simplu",
+    fileName: "chair2.obj",
+    searchTag: "scaun",
+    category: "mobila",
+    thumbnail: "generic link",
+  },
+];
+
+var express = require("express"),
   app = express(),
-  port = process.env.PORT || 3000,
-  bodyParser = require('body-parser');
-  
+  port = process.env.PORT || 3000;
 
+app.get("/models", (req, res) => {
+  res.send(modelData);
+});
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// var routes = require("./routes/modelRoutes"); //importing route
+// routes(app); //register the route
 
-
-var routes = require('./routes/modelRoutes'); //importing route
-routes(app); //register the route
-
-module.exports = app;
+// module.exports = app;
