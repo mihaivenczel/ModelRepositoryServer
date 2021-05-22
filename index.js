1c3b3c9ec83feabdf1207c1f7e77e646784660f5
mihaivenczel <mihaivenczel@yahoo.com>
2021-05-22 23:23:00 +0300

    update

--- index.js
+++ index.js
@@ -21,6 +21,14 @@ app.get("/", (req, res) => {
   res.send("hello world");
 });
 
+app.listen(port, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
+
 // var routes = require("./routes/modelRoutes"); //importing route
 // routes(app); //register the route
 
